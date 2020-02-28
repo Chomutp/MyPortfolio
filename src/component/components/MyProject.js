673a97281bbdaf2f3f02cb22f57b38d9ce91723f
uthumporn naikorn <utp.naikorn@gmail.com>
2020-02-28 23:24:05 +0700

    [Added]:myproject and my information

--- src/component/components/MyProject.js
+++ src/component/components/MyProject.js
@@ -14,46 +14,67 @@ export default class MyProject extends Component {
           </Col>
           <Col xs={24} sm={12} md={12} lg={12} xl={8}>
             <Row justify="center">
-              <Card hoverable title="Card title" className="card-myproject">
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+              <Card
+                hoverable
+                title="BookSwap Project"
+                className="card-myproject"
+              >
+                <img
+                  src="https://i.imgur.com/j2K3zVj.jpg"
+                  className="pic-projects"
+                />
               </Card>
             </Row>
           </Col>
           <Col xs={24} sm={12} md={12} lg={12} xl={8}>
             <Row justify="center">
-              <Card hoverable title="Card title" className="card-myproject">
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+              <Card hoverable title="Group Project" className="card-myproject">
+                <img
+                  src="https://i.imgur.com/Oj0UZn6.png"
+                  className="pic-projects"
+                />
+              </Card>
+            </Row>
+          </Col>
+          <Col xs={24} sm={12} md={12} lg={12} xl={8}>
+            <Row justify="center ">
+              <Card
+                hoverable
+                title="HTML Tribute Page Project"
+                className="card-myproject"
+              >
+                <img
+                  src="https://i.imgur.com/rKrFXXR.jpg"
+                  className="pic-projects"
+                />
               </Card>
             </Row>
           </Col>
           <Col xs={24} sm={12} md={12} lg={12} xl={8}>
             <Row justify="center">
-              <Card hoverable title="Card title" className="card-myproject">
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+              <Card
+                hoverable
+                title="HTML Resume Practice"
+                className="card-myproject"
+              >
+                <img
+                  src="https://i.imgur.com/wlkw6u3.png"
+                  className="pic-projects"
+                />
               </Card>
             </Row>
           </Col>
           <Col xs={24} sm={12} md={12} lg={12} xl={8}>
             <Row justify="center">
-              <Card hoverable title="Card title" className="card-myproject">
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
-            </Row>
-          </Col>
-          <Col xs={24} sm={12} md={12} lg={12} xl={8}>
-            <Row justify="center">
-              <Card hoverable title="Card title" className="card-myproject">
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+              <Card
+                hoverable
+                title="Calculator with Redux"
+                className="card-myproject"
+              >
+                <img
+                  src="https://i.imgur.com/5gRH3TZ.png"
+                  className="pic-projects"
+                />
               </Card>
             </Row>
           </Col>
